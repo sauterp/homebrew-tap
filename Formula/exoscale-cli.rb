ecd4905b8b086d4c662a5a5072080efeed32f9dc
Yoan Blanc <yoan.blanc@exoscale.ch>
2019-01-11 12:51:20 +0100

    exoscale-cli: v1.0.6

--- Formula/exoscale-cli.rb
+++ Formula/exoscale-cli.rb
@@ -1,9 +1,9 @@
 class ExoscaleCli < Formula
   desc "Manage (almost) everything at Exoscale from the command line."
   homepage "https://exoscale.github.io/cli/"
-  url "https://github.com/exoscale/cli/releases/download/v1.0.5/exoscale-cli_1.0.5_darwin_amd64.tar.gz"
-  version "1.0.5"
-  sha256 "7de964bea65742bc85b0e6994d6d80cab3990a208098fde9f64fe254b2af076c"
+  url "https://github.com/exoscale/cli/releases/download/v1.0.6/exoscale-cli_1.0.6_darwin_amd64.tar.gz"
+  version "1.0.6"
+  sha256 "413add3a3a76a193ab72ecb2adb1964ec2ca657d789749091be7a544b2b2feb5"
 
   def install
     bin.install "exo"
