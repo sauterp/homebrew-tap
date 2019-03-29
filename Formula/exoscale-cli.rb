936a04d95bfdab06fc3a6de1db2e46fa228b53bb
Pierre-Emmanuel Jacquier <pierre-emmanuel.jacquier@epitech.eu>
2019-03-29 15:26:23 +0100

    exoscale-cli: v1.1.4
    Signed-off-by: Pierre-Emmanuel Jacquier <pierre-emmanuel.jacquier@epitech.eu>

--- Formula/exoscale-cli.rb
+++ Formula/exoscale-cli.rb
@@ -1,9 +1,9 @@
 class ExoscaleCli < Formula
   desc "Manage (almost) everything at Exoscale from the command line."
   homepage "https://exoscale.github.io/cli/"
-  url "https://github.com/exoscale/cli/releases/download/v1.1.3/exoscale-cli_1.1.3_darwin_amd64.tar.gz"
-  version "1.1.3"
-  sha256 "28a5a5459a1ae24ad111fe1c3da3ef91334b14f8afc08e6d75c029b93aa24d03"
+  url "https://github.com/exoscale/cli/releases/download/v1.1.4/exoscale-cli_1.1.4_darwin_amd64.tar.gz"
+  version "1.1.4"
+  sha256 "23cce0d7cb0e1aa75a5f43d106350667cc0c476fcc82418674a94bb398b17a35"
 
   def install
     bin.install "exo"
