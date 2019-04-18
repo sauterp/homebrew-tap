500b2f6e0daa864885db68083b937d20f1ef902e
Pierre-Emmanuel Jacquier <pierre-emmanuel.jacquier@epitech.eu>
2019-04-18 12:10:45 +0200

    Update exoscale-cli.rb

--- Formula/exoscale-cli.rb
+++ Formula/exoscale-cli.rb
@@ -1,9 +1,9 @@
 class ExoscaleCli < Formula
   desc "Manage (almost) everything at Exoscale from the command line."
   homepage "https://exoscale.github.io/cli/"
-  url "https://github.com/exoscale/cli/releases/download/v1.1.4/exoscale-cli_1.1.4_darwin_amd64.tar.gz"
-  version "1.1.4"
-  sha256 "4a6cc4636bd18f07cf764c3d7829a27093a07c37fdd75a400c2ad008b0bb5dbf"
+  url "https://github.com/exoscale/cli/releases/download/v1.2.0/exoscale-cli_1.2.0_darwin_amd64.tar.gz"
+  version "1.2.0"
+  sha256 "18ea6ff9a2cb3d5e90612bc09c6e6aa6964d88cf4ea3bac5b2622b486efb9ff9"
 
   def install
     bin.install "exo"
