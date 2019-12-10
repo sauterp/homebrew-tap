74a6d51a82eace5585adb392419620f7e2f10ccf
goreleaserbot <goreleaser@carlosbecker.com>
2019-12-10 11:12:32 +0100

    Brew formula update for exoscale-cli version v1.8.0

--- Formula/exoscale-cli.rb
+++ Formula/exoscale-cli.rb
@@ -2,16 +2,16 @@
 class ExoscaleCli < Formula
   desc "Manage (almost) everything at Exoscale from the command line."
   homepage "https://exoscale.github.io/cli/"
-  version "1.7.0"
+  version "1.8.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/exoscale/cli/releases/download/v1.7.0/exoscale-cli_1.7.0_darwin_amd64.tar.gz"
-    sha256 "5f4e775287032b31182fa0e5ce5c1b3f8f21c6628612f2e9f49ba5d709b12bab"
+    url "https://github.com/exoscale/cli/releases/download/v1.8.0/exoscale-cli_1.8.0_darwin_amd64.tar.gz"
+    sha256 "03660859b04fc0d1fbcb53d6e4403f3b05d9e0bfc5f53eba33cca57075ae6302"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/exoscale/cli/releases/download/v1.7.0/exoscale-cli_1.7.0_linux_amd64.tar.gz"
-      sha256 "10a487c1235901ab27834ef4ae0e04f4b29f6ecf7e2eb58b6f2d4f98e0baa2bc"
+      url "https://github.com/exoscale/cli/releases/download/v1.8.0/exoscale-cli_1.8.0_linux_amd64.tar.gz"
+      sha256 "faf98d00f5c37bffd53abbe5d6cf44dcf9893f0efe4f173307b57d1d5621b147"
     end
   end
 
