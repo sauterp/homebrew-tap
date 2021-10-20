c45b8262a911b4018e335e94df387a56f0e79928
goreleaserbot <goreleaser@carlosbecker.com>
2021-10-20 15:50:22 +0200

    Brew formula update for exoscale-cli version v1.45.2

--- Formula/exoscale-cli.rb
+++ Formula/exoscale-cli.rb
@@ -5,26 +5,26 @@
 class ExoscaleCli < Formula
   desc "Manage easily your Exoscale infrastructure from the command-line."
   homepage "https://exoscale.github.io/cli/"
-  version "1.45.1"
+  version "1.45.2"
   bottle :unneeded
 
   on_macos do
-    url "https://github.com/exoscale/cli/releases/download/v1.45.1/exoscale-cli_1.45.1_darwin_all.tar.gz"
-    sha256 "51770c5df8f1e64cdf831177ad5753acec70800112594b51088e3c5b368c4edc"
+    url "https://github.com/exoscale/cli/releases/download/v1.45.2/exoscale-cli_1.45.2_darwin_all.tar.gz"
+    sha256 "6d7def1d66a52e7d6b0297e8eeefbc1b76b2a39dc364be30800c10344cb1b3e6"
   end
 
   on_linux do
     if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/exoscale/cli/releases/download/v1.45.1/exoscale-cli_1.45.1_linux_armv6.tar.gz"
-      sha256 "31416a27590595ffe7ad17f5e08c28dec1e20ae206f1e55365fc28678cbd3d33"
+      url "https://github.com/exoscale/cli/releases/download/v1.45.2/exoscale-cli_1.45.2_linux_armv6.tar.gz"
+      sha256 "4e6b7dae4813323acbf10ede3c6ff1ed501ef83ce514d58f3652b3903f96d255"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/exoscale/cli/releases/download/v1.45.1/exoscale-cli_1.45.1_linux_arm64.tar.gz"
-      sha256 "e133fca2f1825bbe9396f9181d005a20447a47d76f460171b08fbbc1b731f8a7"
+      url "https://github.com/exoscale/cli/releases/download/v1.45.2/exoscale-cli_1.45.2_linux_arm64.tar.gz"
+      sha256 "6cca3e456be5a1fa48449c9408e3235e9182137ec14822492a14d502de164788"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/exoscale/cli/releases/download/v1.45.1/exoscale-cli_1.45.1_linux_amd64.tar.gz"
-      sha256 "9db53c224e9b9be6dae2a24207c9c729ea0156672c0cc48b193b4bc484b9e29c"
+      url "https://github.com/exoscale/cli/releases/download/v1.45.2/exoscale-cli_1.45.2_linux_amd64.tar.gz"
+      sha256 "b6d58769fc90e40e1e1d8fe1bb9a8fe87585ce46e2965ede7a66037d044e0650"
     end
   end
 
